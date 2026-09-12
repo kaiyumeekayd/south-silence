352f530fa569841cf1608ff32aa094c6c1765947
kaiyumeekayd <kaiyumeekayd@gmail.com>
2026-09-12 07:42:03 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -6,7 +6,6 @@
 #define SCREEN_HEIGHT 240
 
 #define FOCAL_LENGTH 256
-
 #define OT_LENGTH 16
 
 typedef struct
@@ -18,7 +17,7 @@ typedef struct
 
 typedef struct
 {
-    POLY_F3 polygons[2];
+    POLY_F3 polygons[12];
     uint32_t ot[OT_LENGTH];
 } RenderContext;
 
@@ -30,6 +29,28 @@ static int project_x(Vertex3D v)
 static int project_y(Vertex3D v)
 {
     return 120 - (int)((v.y * FOCAL_LENGTH) / v.z);
+}
+
+static void make_triangle(
+    POLY_F3 *poly,
+    Vertex3D a,
+    Vertex3D b,
+    Vertex3D c,
+    int r,
+    int g,
+    int bcol
+)
+{
+    setPolyF3(poly);
+
+    setRGB0(poly, r, g, bcol);
+
+    setXY3(
+        poly,
+        project_x(a), project_y(a),
+        project_x(b), project_y(b),
+        project_x(c), project_y(c)
+    );
 }
 
 int main(void)
@@ -67,93 +88,138 @@ int main(void)
     SetDispMask(1);
 
     /*
-     * QUATRO VÉRTICES 3D
+     * CUBO 3D
+     *
+     * Frente: Z = 350
+     * Trás:   Z = 500
      */
 
-    Vertex3D v0 = { -90,  70, 350 };
-    Vertex3D v1 = {  90,  70, 350 };
-    Vertex3D v2 = {  70, -70, 500 };
-    Vertex3D v3 = { -70, -70, 500 };
+    Vertex3D v[8] =
+    {
+        /* Frente */
+        { -70,  70, 350 },
+        {  70,  70, 350 },
+        {  70, -70, 350 },
+        { -70, -70, 350 },
+
+        /* Trás */
+        { -70,  70, 500 },
+        {  70,  70, 500 },
+        {  70, -70, 500 },
+        { -70, -70, 500 }
+    };
 
     while (1)
     {
-        ClearOTagR(
-            ctx.ot,
-            OT_LENGTH
-        );
+        ClearOTagR(ctx.ot, OT_LENGTH);
 
         /*
-         * TRIÂNGULO 1
+         * FACE DA FRENTE
          */
 
-        setPolyF3(&ctx.polygons[0]);
-
-        setRGB0(
+        make_triangle(
             &ctx.polygons[0],
-            140,
-            140,
-            140
+            v[0], v[1], v[2],
+            140, 140, 140
         );
 
-        setXY3(
-            &ctx.polygons[0],
-
-            project_x(v0),
-            project_y(v0),
-
-            project_x(v1),
-            project_y(v1),
-
-            project_x(v2),
-            project_y(v2)
-        );
-
-        /*
-         * TRIÂNGULO 2
-         */
-
-        setPolyF3(&ctx.polygons[1]);
-
-        setRGB0(
+        make_triangle(
             &ctx.polygons[1],
-            100,
-            100,
-            100
-        );
-
-        setXY3(
-            &ctx.polygons[1],
-
-            project_x(v0),
-            project_y(v0),
-
-            project_x(v2),
-            project_y(v2),
-
-            project_x(v3),
-            project_y(v3)
+            v[0], v[2], v[3],
+            140, 140, 140
         );
 
         /*
-         * Os dois polígonos entram
-         * no nível 1 da Ordering Table.
+         * FACE DE TRÁS
          */
 
-        addPrim(
-            &ctx.ot[1],
-            &ctx.polygons[0]
+        make_triangle(
+            &ctx.polygons[2],
+            v[4], v[6], v[5],
+            60, 60, 60
         );
 
-        addPrim(
-            &ctx.ot[1],
-            &ctx.polygons[1]
+        make_triangle(
+            &ctx.polygons[3],
+            v[4], v[7], v[6],
+            60, 60, 60
         );
 
         /*
-         * A partir daqui usamos o último
-         * nível da Ordering Table como
-         * ponto inicial da GPU.
+         * LADO ESQUERDO
          */
+
+        make_triangle(
+            &ctx.polygons[4],
+            v[0], v[3], v[7],
+            90, 90, 90
+        );
+
+        make_triangle(
+            &ctx.polygons[5],
+            v[0], v[7], v[4],
+            90, 90, 90
+        );
+
+        /*
+         * LADO DIREITO
+         */
+
+        make_triangle(
+            &ctx.polygons[6],
+            v[1], v[5], v[6],
+            110, 110, 110
+        );
+
+        make_triangle(
+            &ctx.polygons[7],
+            v[1], v[6], v[2],
+            110, 110, 110
+        );
+
+        /*
+         * TOPO
+         */
+
+        make_triangle(
+            &ctx.polygons[8],
+            v[0], v[4], v[5],
+            120, 120, 120
+        );
+
+        make_triangle(
+            &ctx.polygons[9],
+            v[0], v[5], v[1],
+            120, 120, 120
+        );
+
+        /*
+         * BASE
+         */
+
+        make_triangle(
+            &ctx.polygons[10],
+            v[3], v[2], v[6],
+            70, 70, 70
+        );
+
+        make_triangle(
+            &ctx.polygons[11],
+            v[3], v[6], v[7],
+            70, 70, 70
+        );
+
+        /*
+         * Coloca os 12 polígonos na OT.
+         */
+
+        for (int i = 0; i < 12; i++)
+        {
+            addPrim(
+                &ctx.ot[1],
+                &ctx.polygons[i]
+            );
+        }
 
         DrawOTag(
             &ctx.ot[OT_LENGTH - 1]
