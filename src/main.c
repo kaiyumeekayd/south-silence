a88663ad64ac955017d57ad764c4dc0a6e205ac6
kaiyumeekayd <kaiyumeekayd@gmail.com>
2026-09-12 05:04:20 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,17 +1,14 @@
-#include <psx.h>
+#include <sys/types.h>
+#include <psxgpu.h>
+#include <psxgte.h>
+#include <psxapi.h>
 
 int main(void)
 {
     ResetGraph(0);
 
-    FntLoad(960, 256);
-
     while (1)
     {
-        FntPrint(-1, "SOUTH SILENCE");
-        FntPrint(-1, "\nPS1 PROTOTYPE");
-        FntPrint(-1, "\n\nSYSTEM ONLINE");
-
         VSync(0);
     }
 
