e3812b7b1865ae3eaa9b8a047f994384e7d0de87
kaiyumeekayd <kaiyumeekayd@gmail.com>
2026-09-12 06:11:01 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,31 +3,53 @@
 #include <psxgte.h>
 #include <psxapi.h>
 
+#define OT_LEN 8
+
+static DISPENV disp;
+static DRAWENV draw;
+
+static u_long ot[OT_LEN];
+static POLY_F4 poly;
+
 int main(void)
 {
-    DISPENV disp;
-    DRAWENV draw;
-
     ResetGraph(0);
 
     SetDefDispEnv(&disp, 0, 0, 320, 240);
     SetDefDrawEnv(&draw, 0, 0, 320, 240);
 
-    draw.r0 = 80;
-    draw.g0 = 0;
-    draw.b0 = 0;
     draw.isbg = 1;
+    draw.r0 = 20;
+    draw.g0 = 20;
+    draw.b0 = 20;
 
     PutDispEnv(&disp);
     PutDrawEnv(&draw);
 
     SetDispMask(1);
 
-    DrawSync(0);
+    ClearOTagR(ot, OT_LEN);
+
+    setPolyF4(&poly);
+
+    setRGB0(&poly, 120, 120, 120);
+
+    setXY4(
+        &poly,
+        100, 70,
+        220, 70,
+        240, 190,
+        80, 190
+    );
+
+    addPrim(&ot[OT_LEN - 1], &poly);
 
     while (1)
     {
+        DrawSync(0);
         VSync(0);
+
+        DrawOTag(&ot[0]);
     }
 
     return 0;
