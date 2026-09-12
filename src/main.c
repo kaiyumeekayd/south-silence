00063ed84c8b9495b2f52e850c6e5c8cc04b684b
kaiyumeekayd <kaiyumeekayd@gmail.com>
2026-09-12 07:10:53 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -9,22 +9,39 @@ int main(void)
     DISPENV disp;
     DRAWENV draw;
 
-    /* Inicializa o GPU */
     ResetGraph(0);
 
-    /* Configura tela */
     SetDefDispEnv(&disp, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
     SetDefDrawEnv(&draw, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
-    /* Fundo vermelho */
-    setRGB0(&draw, 100, 0, 0);
+    setRGB0(&draw, 12, 12, 16);
     draw.isbg = 1;
 
     PutDispEnv(&disp);
     PutDrawEnv(&draw);
 
-    /* Liga a saída de vídeo */
     SetDispMask(1);
+
+    uint32_t ot[1];
+
+    ClearOTagR(ot, 1);
+
+    POLY_F3 triangle;
+
+    setPolyF3(&triangle);
+
+    setRGB0(&triangle, 100, 100, 100);
+
+    setXY3(
+        &triangle,
+        80, 180,
+        160, 60,
+        240, 180
+    );
+
+    addPrim(&ot[0], &triangle);
+
+    DrawOTag(&ot[0]);
 
     while (1)
     {
